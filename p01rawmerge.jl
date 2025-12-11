abfc9987609604a6871f968a6c4441c2cd4ea7ca
Dr. Kostas Alexandridis <ktalexan@outlook.com>
2025-12-11 08:47:42 -0800

    refactor: delete oct-DRKSURL7.jl which defined the project metadata function.

--- p01rawmerge.jl
+++ p01rawmerge.jl
@@ -57,8 +57,31 @@ println("\n2. Import Raw Data (initialization)")
 println("\n2.1. Importing Raw Data Files from disk")
 
 # Creating a new data frame to hold the merged raw data
-mergedrawdf = DataFrame()
+dfdata = DataFrame()
 # Adding column names to the merged data frame
-mergedrawdf[!, "CrashID"] = String[]
-mergedrawdf[!, "Date"] = Date[]
-mergedrawdf[!, "Time"] = String[]
+dfdata[!, "year"] = String[]
+dfdata[!, "datestart"] = Date[]
+dfdata[!, "dateend"] = Date[]
+dfdata[!, "cntcrashes"] = Int64[]
+dfdata[!, "cntparties"] = Int64[]
+dfdata[!, "cntvictims"] = Int64[]
+
+# Set a new index for the data frame
+i = 1
+
+# Initialize variables
+data = nothing
+datestart = prjmd["datestart"]
+dateend = prjmd["dateend"]
+cntcrashes = 0
+cntparties = 0
+cntvictims = 0
+
+# Loop through the years in the project metadata
+for year in collect(prjmd["years"])
+    # set the index for the data frame year
+    dfdata[i, "year"] = year
+    # Loop through the levels of the data frame
+    for level in ["Crashes", "Parties", "Victims"]
+        # Read the data from the CSV file
+        
