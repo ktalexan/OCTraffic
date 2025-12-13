0edb904e4b4b5d27da9e22056f2f3d76c2032386
Kostas Alexandridis <ktalexan@outlook.com>
2025-12-12 18:09:41 -0800

    feat: Add and reorder `cid` columns in crash, party, and victim dataframes.

--- p02rawprocess.jl
+++ p02rawprocess.jl
@@ -503,3 +503,20 @@ for df in [crashes, parties, victims]
     end
 end
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## 3.3. Add CID, PID and VID Columns ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+println("\n3.3. Add CID, PID and VID Columns")
+
+### Add CID Columns ----
+println("- Adding CID columns...")
+# Generate CID column for the data frames by converting the case_id column to a string and adding them to the data frame
+crashes[!, "cid"] = string.(crashes[!, "caseid"])
+parties[!, "cid"] = string.(parties[!, "caseid"])
+victims[!, "cid"] = string.(victims[!, "caseid"])
+
+# Move the cid column after the caseid column in the data frames
+select!(crashes, "caseid", "cid", Not(["caseid", "cid"]))
+select!(parties, "caseid", "cid", Not(["caseid", "cid"]))
+select!(victims, "caseid", "cid", Not(["caseid", "cid"]))
